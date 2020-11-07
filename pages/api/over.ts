192457e361c34e1e51d515aece72d710f43df78b
Ian Grossberg <yo.ian.g@gmail.com>
2020-11-07 12:22:23 -0500

    Election results

--- pages/api/over.ts
+++ pages/api/over.ts
@@ -3,7 +3,12 @@ import { Over, NotOver } from '../../model/IsItFinallyOver'
 
 export const over = () : Over | NotOver => {
   return {
-    over: false
+    over: true,
+    sources: [
+      ["associated press", "https://apnews.com/article/joe-biden-wins-white-house-ap-fd58df73aa677acb74fce2a69adb71f9"],
+      ["fox news", "https://www.foxnews.com/politics/biden-wins-presidency-trump-fox-news-projects"],
+      ["new york times", "https://www.nytimes.com/interactive/2020/11/03/us/elections/results-president.html"]
+    ]
   }
 }
 
